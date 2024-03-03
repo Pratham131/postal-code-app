9beb24273653e458321d08421a2351abcd38f0e3
Pratham131 <prathamakhand2000@gmail.com>
2024-03-04 00:36:36 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import myImage from "./images/Vector.png";
 
 function App() {
   const [pincode, setPincode] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [response, setResponse] = useState([]);
-  const [message, setMessage] = useState("");
-  const [selectedPincode, setSelectedPincode] = useState();
-  const [filterOffice, setFilterOffice] = [];
   const [filter, setFilter] = useState("");
+  const [show, setShow] = useState(true);
 
   const handlePincode = (e) => {
     setPincode(e.target.value);
@@ -26,112 +24,91 @@ function App() {
 
   const fetchData = async () => {
     try {
+      setIsLoading(true);
       if (pincode.length === 6) {
         const response = await axios.get(
           `https://api.postalpincode.in/pincode/${pincode}`
         );
         const data = response.data;
-        console.log(response.data)
-        // console.log(data[0].Status)
-        // console.log(data[0].PostOffice);
-        // console.log(data[0].Message)
+        // console.log(response.data);
         if (data[0].Status === "Success") {
           setResponse(data);
-          // setMessage(data[0].Message);
-          // setSelectedPincode(pincode);
-          // console.log(data)
+          setShow(false);
         } else {
           setError(data[0].Message);
-          // console.log(data[0].Message)
         }
       } else {
         alert("Postal code should be 6 digits.");
       }
+      setIsLoading(false);
     } catch (error) {
-      return setError(error);
+      setError(error);
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    const filteredResponse =
-      response.length > 0 && response[0].PostOffice
-        ? response[0].PostOffice.filter((office) =>
-            office.Name.toLowerCase().includes(filter)
-          )
-        : [];
-    setResponse([{ ...response[0], PostOffice: filteredResponse }]);
+    if (response.length > 0) {
+      const filteredResponse =
+        response[0].PostOffice &&
+        response[0].PostOffice.filter((office) =>
+          office.Name.toLowerCase().includes(filter)
+        );
+      if (
+        JSON.stringify(filteredResponse) !==
+        JSON.stringify(response[0].PostOffice)
+      ) {
+        setResponse([{ ...response[0], PostOffice: filteredResponse }]);
+      }
+    }
   }, [filter, response]);
 
   return (
     <div className="App">
-      <div className="container">
+      {show && (
         <div className="form-container">
           <label>Enter Pincode</label>
           <input type="text" placeholder="Pincode" onChange={handlePincode} />
           <button onClick={fetchData}>Lookup</button>
           {error && <p className="err-p">Error: {error}</p>}
         </div>
+      )}
 
-        {/* <div>
-          {response && (
-            <div className="card-container">
-              <h3>Pincode: {pincode}</h3>
-              {message && <p>Message: {message}</p>}
-
-              <div>
-                <img src={myImage} />
-                <input
-                  type="text"
-                  placeholder="Filter"
-                  value={filter}
-                  onChange={handleFilter}
-                />
-              </div>
-
-              {response.map((office) => (
-                <div className="office-card">
-                  <h3>Name: {office.Name}</h3>
-                  <p>Branch Type: {office.BranchType}</p>
-                  <p>Delivery Status: {office.DeliveryStatus}</p>
-                  <p>District: {office.District}</p>
-                  <p>State: {office.State}</p>
-                </div>
-              ))}
-            </div>
-          )}
-        </div> */}
-
-<div>
-          { response.length > 0 && (
-            
-            <div className="card-container">
-              <h3>Pincode: {pincode}</h3>
-              <p>Message: {response[0].Message}</p>
-
-              <div>
-                <img src={myImage} />
-                <input
-                  type="text"
-                  placeholder="Filter"
-                  value={filter}
-                  onChange={handleFilter}
-                />
-              </div>
-
-              {response[0].PostOffice.map((office) => (
-                <div className="office-card">
-                  <h3>Name: {office.Name}</h3>
-                  <p>Branch Type: {office.BranchType}</p>
-                  <p>Delivery Status: {office.DeliveryStatus}</p>
-                  <p>District: {office.District}</p>
-                  <p>State: {office.State}</p>
-                </div>
-              ))}
-            </div>
-          )}
+      {isLoading && (
+        <div className="loader-container">
+          <div className="loader"></div>
         </div>
-        
-      </div>
+      )}
+
+      {response.length > 0 && (
+        <div className="res-container">
+          <h2>Pincode: {pincode}</h2>
+          <p>Message: {response[0].Message}</p>
+
+          <div className="search-div">
+            <img src={myImage} />
+            <input
+              style={{ border: "none" }}
+              type="text"
+              placeholder="Filter"
+              value={filter}
+              onChange={handleFilter}
+            />
+          </div>
+
+          <div className="card-container">
+            {response[0].PostOffice.map((office) => (
+              <div key={office.Name} className="office-card">
+                <h3>Name: {office.Name}</h3>
+                <p>Branch Type: {office.BranchType}</p>
+                <p>Delivery Status: {office.DeliveryStatus}</p>
+                <p>District: {office.District}</p>
+                <p>State: {office.State}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
